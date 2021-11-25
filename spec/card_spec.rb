40f4687d108e8f6b2cf18bd045b9bf03f4b6655d
kimjinsoo <roorooro9933@gmail.com>
2021-11-25 21:46:58 +0900

    understanding before method in rspec

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -28,14 +28,26 @@ RSpec.describe 'Card' do
   # 로컬 변수라고 생각하면 됨
   # 그래서 이렇게 card를 따로 만들긴 했는데 리팩토링 해야 함.
 
+  # before
+  # before(:example)
+  # before(:suite)
+  # 뭐 등등 많긴 한데 아래처럼 적으면 모든 it이 돌기전에 다시 이 before문을 실행시킨다고 이해하면 된다.
+  before do
+    # instance varialbe로 만들어 줘야함.
+    # 근데 한 번만 그럼 초기화 시켜줘야 되는거 아닌가.
+    # 각각의 it마다 instance variable이 초기화 되어야 한다는 것 같다.
+    # 뭐 그렇게 이해해도 될 것 같다.
+    # 단순히 card만 하더라도 괜찮은 것 같지만 그렇게 하면 안된다는 사실.
+    # 근데 궁금한건 module이나 class가 아닌데 instance variable을 사용할 수 있는거임??
+    @card = Card.new('Ace', 'Spades')
+  end
+
     it 'has a rank' do
-      card = Card.new('Ace', 'Spades')
-      expect(card.rank).to eq('Ace')
+      expect(@card.rank).to eq('Ace')
     end
 
     it 'has a rank' do
-      card = Card.new('Ace', 'Spades')
-      expect(card.suit).to eq('Spades')
+      expect(@card.suit).to eq('Spades')
     end
 
 end
