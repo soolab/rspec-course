64322c247e4d544d172b3ebba2ab666f3e754c05
kimjinsoo <roorooro9933@gmail.com>
2021-11-25 21:27:39 +0900

    mutiple it in one group and same card problem

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,7 +1,38 @@
 # frozen_string_literal: true
 
-# RSpec.describe('Card')
-# do, end는 그룹을 만들어주는 것이라고 생각하면 됨.
+class Card
+
+  attr_accessor :rank, :suite
+
+  def initialize(rank, suite)
+    @rank = rank
+    @suite = suite
+  end
+end
+
 RSpec.describe 'Card' do
 
+  # 'it' is a kind of method
+  # it take stirng as a first parameter
+  # and take do end for yield
+
+  it 'has a type' do
+    # card = Card.new('Ace of Spades')
+    # expect(1+1).to eq(2)
+    # it is described in .rspec file
+    # eq is a parameter of to method!
+    # expect(card.type).to(eq('Ace of Spades'))
+    # below is the same with above
+    # expect(card.type).to eq('Ace of Spades')
+
+    it 'has a rank' do
+      card = Card.new('Ace', 'Spades')
+      expect(card.rank).to eq('Ace')
+    end
+
+    it 'has a rank' do
+      card = Card.new('Ace', 'Spades')
+      expect(card.suite).to eq('Spades')
+    end
+  end
 end
